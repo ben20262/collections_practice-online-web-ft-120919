5e4322d7382c8f23816c88ae86f9269b496c48e0
Benjamin Small <ben20262@aim.com>
2019-12-11 01:06:10 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,11 +15,10 @@ def sort_array_char_count (array)
 end
 
 def swap_elements (array)
-  array.sort do |a, b|
-    if a == array[1] && b == array[2]
-      -1
-    end
-  end
+  first = array[1]
+  array[1] = array[2]
+  array[2] = first
+  array
 end
 
 def swap_elements_from_to (array, index, dest)
