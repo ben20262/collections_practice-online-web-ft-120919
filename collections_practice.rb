adbb6e9a8147721fcc95b250fa8f3cf3ec620273
Benjamin Small <ben20262@aim.com>
2019-12-11 00:39:31 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,26 +1,36 @@
-def sort_array_asc
+def sort_array_asc (array)
+  array.sort
 end
 
-def sort_array_desc
+def sort_array_desc (array)
+  array.sort.reverse
 end
 
-def sort_array_char_count
+def sort_array_char_count (array)
+  array.sort do |a, b|
+    a.size <=> b.size
+  end
 end
 
-def swap_elements
+def swap_elements (array)
+  array.sort do |a, b|
+    if a == array[1] && b == array[2]
+      -1
+    end
+  end
 end
 
-def reverse_array
+def reverse_array (array)
 end
 
-def kesha_maker
+def kesha_maker (array)
 end
 
-def find_a
+def find_a (array)
 end
 
-def sum_array
+def sum_array (array)
 end
 
-def ass_s
+def ass_s (array)
 end
